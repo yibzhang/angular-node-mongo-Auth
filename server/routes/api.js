62118942869b64276e01151b309ecce1c3a368a9
yibzhang <zhangyiboturku@gmail.com>
2019-05-14 23:21:50 +0300

    add token verify

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router  = express.Router()
 const User    = require('../models/user')
+const jwt     = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const db       = 'mongodb+srv://yibo:dO60x20ak3CO6uaF@cluster0-7crlb.mongodb.net/test?retryWrites=true'
 
+const tokenKey = "whatever"
 mongoose.connect(db, error=>{
 	error ? console.log("error " + error) : console.log("DB connect successfully")
 })
@@ -16,7 +18,10 @@ router.post('/register', (req, res)=>{
 	let registerUser = req.body
 	let user         = new User(registerUser)
 	user.save((error, registeredUser) => {
-		error ? console.log(error) : res.status(200).send(registeredUser)
+		error ? console.log(error) 
+		: (payload = { subject: registerUser._id},		
+		   token   = jwt.sign(payload, tokenKey),
+		   res.status(200).send({"token":token, "email":user.email}))
 	})
 })
 
@@ -24,9 +29,20 @@ router.post('/login', (req, res)=>{
 	let loginUser = req.body
 
 	User.findOne({email: loginUser.email}, (error, user)=>{
-		user ? (loginUser.password == user.password ? res.status(200).send(user) : res.status(401).send("Wrong password"))
+		user ? (loginUser.password == user.password ? (payload = {subject:user._id}, token = jwt.sign(payload, tokenKey),res.status(200).send({"token":token, "email":user.email})) 
+				: res.status(401).send("Wrong password"))
 		: (res.status(401).send("Invalid email"))
 	})
+})
+
+router.put('/update', (req, res)=>{
+	if(!req.headers.authorization) {
+		return res.status(401).send("Unauthorized request")
+	}
+	let token   = req.headers.authorization.split(' ')[1]
+	let payload = jwt.verify(token, tokenKey)
+	console.log(payload.subject)
+	res.status(200).send("Updating user feature not yet finished");
 })
 
 module.exports = router
